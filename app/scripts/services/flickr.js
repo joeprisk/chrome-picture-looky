ea85c33114ed103b0bcc4150823b66eb2e79b39e
joe.prisk <joe.prisk@uknetweb.com>
2014-08-28 17:08:16 +0100

    nitial git commit + flickr api key

--- app/scripts/services/flickr.js
+++ app/scripts/services/flickr.js
@@ -4,10 +4,14 @@ angular.module('flickrSearchApp')
   .service('flickr', ['$rootScope','$http', function ($rootScope, $http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
+		var options = {
+			key : "b044f705296205f71f35f96c5273129d",
+			secret : "9402706679edf869"
+		};
         function flickrPull(search, page, broadcast) {
 
-            var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&' +
-                'api_key=9ca70fb67b982023dfe10397e3afc625&' +
+            var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&' +
+                'api_key=' + options.key + '&' +
                 'text=' + encodeURIComponent(search) + '&' +
                 'per_page=20&page='+ page + '&' +
                 'format=json&nojsoncallback=1&';
